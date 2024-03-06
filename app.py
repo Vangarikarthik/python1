8593157edf5947c5e8fe21ad78ae203709f23ba6
Vangarikarthik <vangarinani@gmail.com>
2024-03-06 21:57:56 +0530

    changed code

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 def calculate_sum():
     try:
-        num1 = float(input("Enter the first number: "))
-        num2 = float(input("Enter the second number: "))
+        num1 = 59
+        num2 = 10
         result1 = num1 + num2
         print("The sum of {num1} and {num2} is: {result1}")
     except ValueError:
